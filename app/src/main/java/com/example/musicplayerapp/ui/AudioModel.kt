8356723c52c658c5d18878439af45f0c218215f9
hannamkrtchian <104131383+hannamkrtchian@users.noreply.github.com>
2023-12-14 18:35:02 +0100

    added class MusicListAdapter

--- app/src/main/java/com/example/musicplayerapp/ui/AudioModel.kt
+++ app/src/main/java/com/example/musicplayerapp/ui/AudioModel.kt
@@ -1,5 +1,5 @@
 package com.example.musicplayerapp.ui
 
-public class AudioModel(var path: String, var title: String, var duration: String) {
+class AudioModel(var path: String, var title: String, var duration: String) {
 
 }
---
+++ app/src/main/java/com/example/musicplayerapp/ui/MusicListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.musicplayerapp.ui
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MusicListAdapter {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
