8e3fc21791602a20f19ea233d5d81bf2f4655b8c
hannamkrtchian <104131383+hannamkrtchian@users.noreply.github.com>
2023-12-29 22:29:11 +0100

    initialized components of ShowSongListActivity

--- app/src/main/java/com/example/musicplayerapp/ui/ShowSongListActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/ui/ShowSongListActivity.kt
@@ -2,13 +2,33 @@ package com.example.musicplayerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayerapp.R
 
 class ShowSongListActivity : AppCompatActivity() {
     var currentIndex: Int = -1
 
+    private lateinit var back: ImageView
+    private lateinit var titleTv: TextView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var textViewNoSongs: TextView
+    private lateinit var edit: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_song_list)
+
+        back= findViewById(R.id.back_button)
+        titleTv= findViewById(R.id.playlist_name)
+        recyclerView= findViewById(R.id.recycler_view_songs_playlist)
+        textViewNoSongs = findViewById(R.id.no_songs)
+        edit = findViewById(R.id.edit_button)
+
+        // Back button logic
+        back.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
